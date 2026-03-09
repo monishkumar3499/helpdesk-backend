aef1127b38dc8371baeac168fa59fa59cee78199
Undabatla Gunashekar <undabatlagunashekar@jmangroup.com>
2026-03-09 14:54:44 +0530

    feat: API integration

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -16,9 +16,58 @@ export class UsersController {
     return this.usersService.findAll(role);
   }
 
+  @Get('me/profile')
+  getMyProfile() {
+    // TODO: Get from authentication context
+    // For now, return example user
+    return this.usersService.getUserProfile('example-user-id');
+  }
+
+  @Get('me/tickets')
+  getMyTickets() {
+    // TODO: Get from authentication context
+    return this.usersService.getUserTickets('example-user-id');
+  }
+
+  @Get('me/assets')
+  getMyAssets() {
+    // TODO: Get from authentication context
+    return this.usersService.getUserAssets('example-user-id');
+  }
+
+  @Get('hr/dashboard')
+  getHRDashboard() {
+    return this.usersService.getHRDashboard();
+  }
+
+  @Get('it/dashboard')
+  getITDashboard() {
+    return this.usersService.getITDashboard();
+  }
+
+  @Get('admin/dashboard')
+  getAdminDashboard() {
+    return this.usersService.getAdminDashboard();
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
+  }
+
+  @Get(':id/tickets')
+  getUserTickets(@Param('id') id: string) {
+    return this.usersService.getUserTickets(id);
+  }
+
+  @Get(':id/assets')
+  getUserAssets(@Param('id') id: string) {
+    return this.usersService.getUserAssets(id);
+  }
+
+  @Get(':id/ticket-stats')
+  getEmployeeTicketStats(@Param('id') id: string) {
+    return this.usersService.getEmployeeTicketStats(id);
   }
 
   @Patch(':id/toggle')
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from './generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import type { PrismaClient } from './generated/prisma/client';
+
+// Use require for CJS modules
+const { PrismaClient: PrismaClientBase } = require('./generated/prisma/client');
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClientBase {
   constructor() {
-    const connectionString = 'postgresql://postgres:postgres@localhost:5432/helpdesk';
+    const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:guna@localhost:5432/helpdesk';
     const adapter = new PrismaPg({ connectionString });
     super({ adapter });
   }
